7f1e58e938ce90076b02e767680f1d4842f5ec96
ck06 <ihavelol@hotmail.com>
2021-02-06 08:04:52 +0100

    renamed app module to leveling module

---
+++ src/level.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { LevelController } from './Application/Resource/Controllers/level.controller';
+import { LevelCalculatorService } from './Domain/Resource/level.calculator.service';
+import { RequiredResourcesConverter } from './Domain/Resource/Converters/required.resources.converter';
+import { QualityConverter } from './Infrastructure/Converters/quality.converter';
+
+@Module({
+    imports: [],
+    controllers: [LevelController],
+    providers: [LevelCalculatorService, RequiredResourcesConverter, QualityConverter],
+})
+export class AppModule {}
