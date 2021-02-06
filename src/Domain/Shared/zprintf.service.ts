132cd4c42cce7aa0bc4416b29f81452c691d5928
ck06 <ihavelol@hotmail.com>
2021-02-06 09:51:49 +0100

    fix: typehints in zprintf

--- src/Domain/Shared/zprintf.service.ts
+++ src/Domain/Shared/zprintf.service.ts
@@ -1,11 +1,11 @@
-export function zprintf(format: string, ...replacements: number[] | string[]): string {
+export function zprintf(format: string, ...replacements: (number|string)[]): string {
     if ((format.match(/%[a-z]/gi) || []).length != replacements.length) {
         throw new Error('Not the same amount of replacements as there are replace targets');
     }
 
     replacements.forEach((replacement) => {
-        if (typeof replacement == typeof '') format = format.replace('%s', replacement);
-        if (typeof replacement == typeof 0) format = format.replace('%d', replacement.toString());
+        if (typeof replacement === 'string') format = format.replace('%s', replacement);
+        if (typeof replacement === 'number') format = format.replace('%d', replacement.toString());
     });
 
     return format;
