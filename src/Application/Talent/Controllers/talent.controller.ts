1f49aa88397a85f652e7aa06520b1d9677adb4c8
ck06 <ihavelol@hotmail.com>
2021-02-06 09:18:55 +0100

    feat: talent controller

--- src/Application/Talent/Controllers/talent.controller.ts
+++ src/Application/Talent/Controllers/talent.controller.ts
@@ -1,37 +1,37 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { LevelCalculatorService } from '../../../Domain/Level/Calculator/level.calculator.service';
+import { TalentCalculatorService } from '../../../Domain/Talent/Calculator/talent.calculator.service';
 import { RequiredResourcesConverter } from '../../../Domain/Resource/Converters/required.resources.converter';
 
 @Controller()
-export class LevelController {
+export class TalentController {
     private readonly MIN = 1;
-    private readonly MAX = 90;
+    private readonly MAX = 10;
 
     constructor(
-        private readonly levelCalculator: LevelCalculatorService,
+        private readonly talentCalculator: TalentCalculatorService,
         private readonly resourceConverter: RequiredResourcesConverter,
     ) {}
 
-    @Get('/level/from/:start/to/:end')
+    @Get('/talent/from/:start/to/:end')
     getXToY(@Param('start') start: number, @Param('end') end: number): string {
         if (end > this.MAX || start < this.MIN) {
-            throw Error('levels only range ' + this.MIN.toString() + '~' + this.MAX.toString());
+            throw Error('talents only range ' + this.MIN.toString() + '~' + this.MAX.toString());
         }
 
-        return this.resourceConverter.toJson(this.levelCalculator.calculate(start, end));
+        return this.resourceConverter.toJson(this.talentCalculator.calculate(start, end));
     }
 
-    @Get('/level/to/:end')
+    @Get('/talent/to/:end')
     getToY(@Param('end') end: number): string {
         return this.getXToY(this.MIN, end);
     }
 
-    @Get('/level/from/:start')
+    @Get('/talent/from/:start')
     GetFromX(@Param('start') start: number): string {
         return this.getXToY(start, this.MAX);
     }
 
-    @Get('/level')
+    @Get('/talent')
     getMinToMax(): string {
         return this.getXToY(this.MIN, this.MAX);
     }
