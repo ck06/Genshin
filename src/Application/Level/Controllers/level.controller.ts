373928dc0c30839087856f2c43b72ff99264adc5
ck06 <ihavelol@hotmail.com>
2021-02-10 03:07:09 +0100

    debug: added character routes to start using the entities

--- src/Application/Level/Controllers/level.controller.ts
+++ src/Application/Level/Controllers/level.controller.ts
@@ -1,47 +1,45 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { LevelCalculator } from '../../../Domain/Level/Calculator/level.calculator';
 import { RequiredResourcesConverter } from '../../../Domain/Resource/Converters/required.resources.converter';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Character } from '../../../Infrastructure/Database/Entities/character.entity';
+import { Repository } from 'typeorm';
 
 @Controller()
 export class LevelController {
-    private readonly MIN = 1;
-    private readonly MAX = 90;
+  private readonly MIN = 1;
+  private readonly MAX = 90;
 
-    constructor(
-        private readonly levelCalculator: LevelCalculator,
-        private readonly resourceConverter: RequiredResourcesConverter,
-    ) {}
+  constructor(
+    @InjectRepository(Character, 'SQLite') private characterRepository: Repository<Character>,
+    private readonly levelCalculator: LevelCalculator,
+    private readonly resourceConverter: RequiredResourcesConverter,
+  ) {}
 
-    @Get('/level/from/:start/to/:end')
-    getXToY(@Param('start') start: number, @Param('end') end: number): string {
-        if (end > this.MAX || start < this.MIN) {
-            throw Error(`levels only range ${this.MIN.toString()}~${this.MAX.toString()}`);
-        }
-
-        return JSON.stringify(this.resourceConverter.toSortedObject(this.levelCalculator.calculate(start, end)));
+  @Get('/char/:char/from/:start/to/:end')
+  async getXToY(
+    @Param('char') char: string,
+    @Param('start') start: number,
+    @Param('end') end: number,
+  ): Promise<string> {
+    if (end > this.MAX || start < this.MIN) {
+      return `Levels only range ${this.MIN.toString()}~${this.MAX.toString()}`;
     }
 
-    @Get('/level/to/:end')
-    getToY(@Param('end') end: number): string {
-        return this.getXToY(this.MIN, end);
+    try {
+      char = char.charAt(0).toUpperCase() + char.slice(1);
+      await this.characterRepository.findOneOrFail({ where: { name: char } });
+    } catch {
+      return `Character with the name ${char} could not be found.`;
     }
 
-    @Get('/level/from/:start')
-    GetFromX(@Param('start') start: number): string {
-        return this.getXToY(start, this.MAX);
-    }
+    return JSON.stringify(
+      this.resourceConverter.toSortedObject(await this.levelCalculator.calculate(char, start, end)),
+    );
+  }
 
-    @Get('/level')
-    getMinToMax(): string {
-        return this.getXToY(this.MIN, this.MAX);
-    }
-
-    @Get('/level/pretty')
-    getPretty(): string {
-        return JSON.stringify(
-            this.resourceConverter.toSortedObject(this.levelCalculator.calculate(this.MIN, this.MAX)),
-            null,
-            '\t',
-        );
-    }
+  @Get('/chars/')
+  async getAllCharacters() {
+    return JSON.stringify(await this.characterRepository.find({ where: { quality: 5 } }));
+  }
 }
--- src/Domain/Level/Calculator/level.calculator.ts
+++ src/Domain/Level/Calculator/level.calculator.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import CharacterLevelRequirements from '../../../Infrastructure/Data/Requirements/Character/level.requirements';
-import CharacterAscensionRequirements from '../../../Infrastructure/Data/Requirements/Character/ascension.requirements';
 import RequiredResources from '../../Resource/Models/required.resources';
 import Mora from '../../../Infrastructure/Models/Materials/World/mora';
 import ExperienceBook from '../../../Infrastructure/Models/Materials/World/experience.book';
@@ -8,41 +6,115 @@ import ElementalGem from '../../../Infrastructure/Models/Materials/World/element
 import CommonEnemyDrop from '../../../Infrastructure/Models/Materials/Enemy/common';
 import DailyEnemyDrop from '../../../Infrastructure/Models/Materials/Enemy/daily';
 import GatheredItem from '../../../Infrastructure/Models/Materials/World/gather';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CharacterAscension } from '../../../Infrastructure/Database/Entities/character.ascension.entity';
+import { Repository } from 'typeorm';
+import constants from '../../../Infrastructure/Data/Misc/constants';
+import { CharacterExperience } from '../../../Infrastructure/Database/Entities/character.experience.entity';
+import { Item } from '../../../Infrastructure/Database/Entities/item.entity';
+import { ItemType } from '../../../Infrastructure/Database/Entities/item_type.entity';
 
 @Injectable()
 export class LevelCalculator {
-    public calculate(start: number, end: number): RequiredResources {
-        let totals = new RequiredResources();
-        for (let i = start; i < end; i++) {
-            // TODO: let required exp build up until hitting an ascension level, and THEN determine amount of books.
-            totals.addResource(new Mora(CharacterLevelRequirements.EXP_TO_LEVEL[i] * CharacterLevelRequirements.MORA_PER_EXP));
-            for (let requiredExp = CharacterLevelRequirements.EXP_TO_LEVEL[i], quality = 4; quality > 1; quality--) {
-                let books = Math.floor(requiredExp / CharacterLevelRequirements.EXP_PER_BOOK[quality]);
-                requiredExp %= CharacterLevelRequirements.EXP_PER_BOOK[quality];
-                totals.addResource(new ExperienceBook('', books, quality));
+    constructor(
+      @InjectRepository(CharacterExperience, 'SQLite')
+      private experienceRepository: Repository<CharacterExperience>,
+      @InjectRepository(CharacterAscension, 'SQLite')
+      private ascensionRepository: Repository<CharacterAscension>,
+      @InjectRepository(ItemType, 'SQLite')
+      private itemTypeRepository: Repository<ItemType>,
+    ) {}
 
-                if (quality === 2 && requiredExp > 0) {
-                    totals.addResource(new ExperienceBook('', 1, quality));
+    public async calculate(
+      characterName: string,
+      start: number,
+      end: number,
+    ): Promise<RequiredResources> {
+        const TOTALS = new RequiredResources();
+        const EXP_PER_LEVEL = [...(await this.experienceRepository.find())].map((exp) => exp.expToNext);
+        const EXP_BOOKS = (await this.itemTypeRepository.findOne({ where: { inCode: 'books' } })).items;
+        const ASCENSIONS = [
+            ...(await this.ascensionRepository.find({
+                where: { character: characterName },
+                order: { details: 'ASC' },
+            })),
+        ];
+
+        let cumulativeExp = 0;
+        for (let i = start; i < end; i++) {
+            TOTALS.addResource(new Mora(EXP_PER_LEVEL[i] * constants.MORA_PER_CHARACTER_EXP));
+            cumulativeExp += EXP_PER_LEVEL[i];
+
+            if (constants.ASCENSION_LEVELS.includes(i)) {
+                this.calculateExperience(cumulativeExp, EXP_BOOKS).forEach((book) => {
+                    TOTALS.addResource(book);
+                });
+
+                let characterAscension: CharacterAscension;
+                for (let ascension of ASCENSIONS) {
+                    if (ascension.details.level == i) {
+                        characterAscension = ascension;
+                        break;
+                    }
+                }
+
+                TOTALS.addResource(
+                  new ElementalGem(
+                    characterAscension.gem.name,
+                    characterAscension.details.gemAmount,
+                    characterAscension.details.gemQuality.id,
+                  ),
+                );
+                TOTALS.addResource(
+                  new CommonEnemyDrop(
+                    characterAscension.common.name,
+                    characterAscension.details.commonAmount,
+                    characterAscension.details.commonQuality.id,
+                  ),
+                );
+                TOTALS.addResource(
+                  new DailyEnemyDrop(
+                    characterAscension.boss.name,
+                    characterAscension.details.bossAmount,
+                    characterAscension.details.bossQuality.id,
+                  ),
+                );
+                TOTALS.addResource(
+                  new GatheredItem(
+                    characterAscension.gather.name,
+                    characterAscension.details.gatherAmount,
+                    characterAscension.details.gatherQuality.id,
+                  ),
+                );
+                TOTALS.addResource(new Mora(characterAscension.details.mora));
+            }
+        }
+        return TOTALS;
+    }
+
+    // noinspection JSMethodCanBeStatic
+    private calculateExperience(exp: number, books: Item[]) {
+        const totalBooks: ExperienceBook[] = [];
+        for (let quality = 4; quality > 1; quality--) {
+            let currentBook: Item;
+            for (let book of books) {
+                if (book.quality.id === quality) {
+                    currentBook = book;
+                    break;
                 }
             }
 
-            if (LevelCalculator.isAscensionLevel(i)) {
-                const ascension = CharacterAscensionRequirements.ASCENSION_LEVELS[i];
-                const amount = CharacterAscensionRequirements.ASCENSION_REQUIRED_AMOUNTS[ascension];
-                const quality = CharacterAscensionRequirements.ASCENSION_REQUIRED_QUALITIES[ascension];
+            if (currentBook instanceof Item) {
+                let amount = Math.floor(exp / Number(currentBook.details));
+                exp %= Number(currentBook.details);
+                totalBooks.push(new ExperienceBook(currentBook.name, amount, quality));
 
-                totals.addResource(new ElementalGem('', amount.gems, quality.gems));
-                totals.addResource(new CommonEnemyDrop('', amount.commonDrops, quality.commonDrops));
-                totals.addResource(new DailyEnemyDrop('', amount.eliteDrops, quality.eliteDrops));
-                totals.addResource(new GatheredItem('', amount.gatheredItems, quality.gatheredItems));
-                totals.addResource(new Mora(amount.mora));
+                if (quality === 2 && exp > 0) {
+                    totalBooks.push(new ExperienceBook(currentBook.name, 1, quality));
+                }
             }
         }
 
-        return totals;
-    }
-
-    private static isAscensionLevel(level: number): boolean {
-        return CharacterAscensionRequirements.ASCENSION_LEVELS[level] !== undefined;
+        return totalBooks;
     }
 }
