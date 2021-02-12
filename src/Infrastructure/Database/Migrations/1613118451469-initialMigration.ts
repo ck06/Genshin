dc5591f8f15044f8c51540b09cb0f39f40967d22
ck06 <ihavelol@hotmail.com>
2021-02-12 22:00:24 +0100

    migrationify query file 001 + equalize plurality

--- src/Infrastructure/Database/Migrations/1613118451469-initialMigration.ts
+++ src/Infrastructure/Database/Migrations/1613118451469-initialMigration.ts
@@ -1,16 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class initialMigration1613118451469 implements MigrationInterface {
-  /**
-   DROP TABLE IF EXISTS quality;
-   CREATE TABLE characters (
-   id      INTEGER PRIMARY KEY,
-   name    VARCHAR(255),
-   quality INTEGER REFERENCES quality (id)
-   );
-   * @param queryRunner
-   */
-
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
@@ -64,14 +54,93 @@ export class initialMigration1613118451469 implements MigrationInterface {
         ],
       }),
     );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'weapon',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+            collation: 'NOCASE',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            enum: ['Bow', 'Catalyst', 'Claymore', 'Polearm', 'Sword'],
+          },
+          {
+            name: 'quality',
+            type: 'integer',
+          },
+          {
+            name: 'source',
+            type: 'varchar',
+            length: '255',
+            collation: 'NOCASE',
+          },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['quality'],
+            referencedTableName: 'quality',
+            referencedColumnNames: ['id'],
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'item_type',
+        columns: [
+          { name: 'id', type: 'integer', isGenerated: true, isPrimary: true },
+          { name: 'in_code', type: 'varchar', length: '255' }, // TODO: create abstraction layer to fill requiredResources
+          { name: 'in_data', type: 'varchar', length: '255' }, //       using in_code as a key automagically.
+        ],
+      }),
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'item',
+        columns: [
+          { name: 'id', type: 'integer', isGenerated: true, isPrimary: true },
+          { name: 'name', type: 'varchar', length: '255' },
+          { name: 'quality', type: 'integer' },
+          { name: 'type', type: 'integer' },
+          { name: 'details', type: 'varchar', length: '255', isNullable: true, default: null },
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['quality'],
+            referencedTableName: 'quality',
+            referencedColumnNames: ['id'],
+          },
+          {
+            columnNames: ['type'],
+            referencedTableName: 'item_type',
+            referencedColumnNames: ['id'],
+          },
+        ],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     /** DROP TABLE IF EXISTS `x` */
     await queryRunner.dropTable('quality', true);
-    await queryRunner.dropTable('characters', true);
-    await queryRunner.dropTable('weapons', true);
-    await queryRunner.dropTable('item_types', true);
-    await queryRunner.dropTable('items', true);
+    await queryRunner.dropTable('character', true);
+    await queryRunner.dropTable('weapon', true);
+    await queryRunner.dropTable('item_type', true);
+    await queryRunner.dropTable('item', true);
   }
 }
--- src/Infrastructure/Database/Queries/001_create_tables.sql
+++ src/Infrastructure/Database/Queries/001_create_tables.sql
@@ -4,15 +4,15 @@ CREATE TABLE quality (
   color VARCHAR(10)
 );
 
-DROP TABLE IF EXISTS characters;
-CREATE TABLE characters (
+DROP TABLE IF EXISTS character;
+CREATE TABLE character (
   id      INTEGER PRIMARY KEY,
   name    VARCHAR(255),
   quality INTEGER REFERENCES quality (id)
 );
 
-DROP TABLE IF EXISTS weapons;
-CREATE TABLE weapons (
+DROP TABLE IF EXISTS weapon;
+CREATE TABLE weapon (
   id      INTEGER PRIMARY KEY,
   name    VARCHAR(255),
   type    VARCHAR(25) CHECK (type IN ('Bow', 'Catalyst', 'Claymore', 'Polearm', 'Sword')),
@@ -20,19 +20,19 @@ CREATE TABLE weapons (
   source  VARCHAR(255)
 );
 
-DROP TABLE IF EXISTS item_types;
-CREATE TABLE item_types (
+DROP TABLE IF EXISTS item_type;
+CREATE TABLE item_type (
   id      INTEGER PRIMARY KEY,
   in_code VARCHAR(255), -- the types as they appear in the code (keys in objects)
   in_data VARCHAR(255)  -- the "pretty" version, based on HoneyHunterWorld's categories.
 );
 
-DROP TABLE IF EXISTS items;
-CREATE TABLE items (
+DROP TABLE IF EXISTS item;
+CREATE TABLE item (
   id      INTEGER PRIMARY KEY,
   name    VARCHAR(255),
   quality INTEGER REFERENCES quality (id),
-  type    INTEGER REFERENCES item_types (id),
+  type    INTEGER REFERENCES item_type (id),
   details VARCHAR(255) NULL DEFAULT NULL -- i.e. what element a gem is for, or what days a domain item is up on.
 );
 
